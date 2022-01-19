1dd6d70eea97e15a898413bee1ecdcac4ce873d8
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2022-01-19 11:28:24 +0600

    Initial Commit

---
+++ Shared/GoogleAdSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAdSampleApp.swift
+//  Shared
+//
+//  Created by Asaduzzaman Shuvro on 19/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GoogleAdSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
