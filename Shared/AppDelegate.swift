c50aa824e6fc7a7f9a10f12700a0bae0c2c8d183
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2022-01-20 11:12:31 +0600

    - google ad identifier added - Initialise the ad sdk

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 import UIKit
+import GoogleMobileAds
 
 @main
 class AppDelegate: UIResponder ,UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+
         return true
     }
     
